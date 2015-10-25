bba4a3d261fc49211afea1b8924b6eff50cb9ac5
Hugo Hardy <huguihardy@gmail.com>
2015-10-25 09:48:12 -0300

    Carga inicial

---
+++ Código/src/licencias/Imagen.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package licencias;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+/**
+ *
+ * @author HARDY
+ */
+public class Imagen extends javax.swing.JPanel {
+    int x, y; String name;
+
+    public Imagen(JPanel jPanel1, String image_name) {
+        this.x = jPanel1.getWidth();
+        this.y = jPanel1.getHeight();
+        this.setSize(x, y);
+        name = image_name;
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        ImageIcon Img = new ImageIcon(getClass().getResource("/Imagenes/"+name));
+        g.drawImage(Img.getImage(), 0, 0, x, y, null);
+    }    
+
+}
---
+++ Código/src/licencias/Licencias.java
@@ -0,0 +1,21 @@
+
+package licencias;
+
+import licencias.GUIs.Fm_Principal;
+
+/**
+ *
+ * @author HARDY
+ */
+public class Licencias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fm_Principal fm_Principal = new Fm_Principal();
+        fm_Principal.setVisible(true);
+    }
+    
+}
